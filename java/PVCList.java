ae16f7e367c6a214aff26d73048ceb3adc341550
Vladimir Vivien <vladimir.vivien@gmail.com>
2018-03-04 10:21:21 -0500

    java-client example with watch works, yay

--- java/PVCList.java
+++ java/PVCList.java
@@ -5,6 +5,8 @@ import io.kubernetes.client.apis.CoreV1Api;
 import io.kubernetes.client.models.V1PersistentVolumeClaim;
 import io.kubernetes.client.models.V1PersistentVolumeClaimList;
 import io.kubernetes.client.Pair;
+import io.kubernetes.client.auth.*;
+import io.kubernetes.client.util.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,35 +16,33 @@ import java.util.Map;
 
 public class PVCList {
     public static void main(String[] args) throws IOException, ApiException{
-        // K8S_APISERVER env var can be set by running kubectl proxy
-        String apiServer = System.getenv("K8S_APISERVER");
-        // namespace should default to default
+        // K8S_NAMESPACE defaults to empty
         String namespace = System.getenv("K8S_NAMESPACE");
-        // bearer token
-        String bearerToken = System.getenv("K8S_SERVERTOKEN");
 
-        ApiClient client = Configuration.getDefaultApiClient();
-        if (apiServer == null) {
-            apiServer = "https://localhost:443";
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+
+        // fallback to default namespace
+        if (namespace == null || "".equals(namespace)) {
+            namespace = "default";
         }
 
-        // set authorization headers
-        if (bearerToken != null){
-            client.addDefaultHeader("Authorization", "Bearer "+bearerToken);
-            client.setVerifyingSsl(false);
-        }
         //client.setDebugging(true);
-        client.setBasePath(apiServer);
+
+        CoreV1Api api = new CoreV1Api(client);
+        V1PersistentVolumeClaimList list = api.listNamespacedPersistentVolumeClaim(namespace, null, null, null, null, null, null, null, null, null);
+
+        String apiServer = client.getBasePath();
         System.out.format("%nconnecting to API server %s %n%n", apiServer);
         System.out.println("----- PVCs ----");
-
-        Configuration.setDefaultApiClient(client);
-        CoreV1Api api = new CoreV1Api(client);
-        V1PersistentVolumeClaimList list = api.listPersistentVolumeClaimForAllNamespaces(null, null, null, null, null, null, null, null, null);
+        String template = "%-16s\t%-40s\t%-6s%n";
+        System.out.format(template,"Name", "Volume", "Size");
+        
         for (V1PersistentVolumeClaim item : list.getItems()) {
             String name = item.getMetadata().getName();
             String volumeName = item.getSpec().getVolumeName();
-            System.out.format("%-16s\t%-32s%n",name, volumeName);
+            String size = item.getSpec().getResources().getRequests().get("storage");
+            System.out.format(template,name, volumeName, size);
         }
 
         System.out.println();
---
+++ java/PVCWatch.java
@@ -0,0 +1,141 @@
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.models.V1PersistentVolumeClaim;
+import io.kubernetes.client.models.V1PersistentVolumeClaimList;
+import io.kubernetes.client.Pair;
+import io.kubernetes.client.auth.*;
+import io.kubernetes.client.util.*;
+import io.kubernetes.client.custom.Quantity;
+import io.kubernetes.client.custom.Quantity.Format;
+
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.math.BigDecimal;
+
+public class PVCWatch {
+    public static void main(String[] args) throws IOException{
+        // K8S_NAMESPACE defaults to empty
+        String namespace = System.getenv("K8S_NAMESPACE");
+        if (namespace == null || "".equals(namespace)) {
+            namespace = "default";
+        }
+
+        Quantity maxClaims = Quantity.fromString("150Gi");
+        Quantity totalClaims = Quantity.fromString("0Gi");
+
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+        //client.setDebugging(true);
+
+        String apiServer = client.getBasePath();      
+        System.out.format("%nconnecting to API server %s %n%n", apiServer);
+
+        client.getHttpClient().setReadTimeout(300, TimeUnit.SECONDS);
+        CoreV1Api api = new CoreV1Api(client);
+        V1PersistentVolumeClaimList list = null;
+        try{
+            list = api.listNamespacedPersistentVolumeClaim(namespace, null, null, null, null, null, null, null, null, null);
+        }catch(ApiException apie){
+            System.err.println("Exception when calling CoreV1Api#listNamespacedPersistentVolumeClaim");
+            apie.printStackTrace();
+            System.exit(1);
+        }
+        PVCWatch.printPVCs(list);
+
+        // parse watched events
+        System.out.format("%n----- PVC Watch (max total claims: %s) -----%n", maxClaims.toSuffixedString());
+        try {
+            Watch<V1PersistentVolumeClaim> watch = Watch.createWatch(
+                    client,
+                    api.listNamespacedPersistentVolumeClaimCall(
+                            namespace, null, null, null, null, null, null, null, null, Boolean.TRUE, null, null),
+                    new TypeToken<Watch.Response<V1PersistentVolumeClaim>>() {
+                    }.getType()
+            );
+            // let's watch for total PVC sizes
+            for (Watch.Response<V1PersistentVolumeClaim> item : watch) {
+                V1PersistentVolumeClaim pvc = item.object;
+                String claimSize = null;
+                Quantity claimQuant = null;
+                BigDecimal totalNum = null;
+
+                switch (item.type){
+                    case "ADDED":
+                        claimSize = pvc.getSpec().getResources().getRequests().get("storage");
+                        claimQuant = Quantity.fromString(claimSize);
+                        totalNum = totalClaims.getNumber().add(claimQuant.getNumber());
+                        totalClaims = new Quantity(totalNum, Format.BINARY_SI);
+
+                        System.out.format(
+                            "ADDED: PVC %s added, size %s", 
+                            pvc.getMetadata().getName(), claimSize
+                        );
+
+                        // claim size overage ?
+                        if (totalClaims.getNumber().compareTo(maxClaims.getNumber()) >= 1) {
+                                System.out.format(
+                                    "WARNING: claim overage reached: max %s, at %s%n", 
+                                    maxClaims.toString(), totalClaims.toString()
+                                );
+                                System.out.println("*** Trigger action ***");
+                        }
+                        break;
+                    case "MODIFIED":
+                        System.out.format("MODIFIED: PVC %s added, size %s", pvc.getMetadata().getName(), claimSize);
+                        break;
+                    case "DELETED":
+                         claimSize = pvc.getSpec().getResources().getRequests().get("storage");
+                         claimQuant = Quantity.fromString(claimSize);
+                         totalNum = totalClaims.getNumber().subtract(claimQuant.getNumber());
+                         totalClaims = new Quantity(totalNum, Format.BINARY_SI);
+
+                        System.out.format(
+                            "DELETED: PVC %s removed, size %s", 
+                            pvc.getMetadata().getName(), claimSize
+                        );
+
+                        // size back to normal ?
+                        if (totalClaims.getNumber().compareTo(maxClaims.getNumber()) <= 0) {
+                                System.out.format(
+                                    "INFO: claim usage normal: max %s, at %s%n", 
+                                    maxClaims.toString(), totalClaims.toString()
+                                );
+                        }
+                        break;
+                }
+                System.out.format(
+                    "%nINFO: Total PVC is at %3.1f%% capacity (%s/%s)%n", 
+                    (totalClaims.getNumber().divide(maxClaims.getNumber()).floatValue()) * 100,
+                    totalClaims.toSuffixedString(), 
+                    maxClaims.toSuffixedString()
+                );
+            }
+
+        }catch(ApiException apie) {
+            System.err.println("Exception watching PersistentVolumeClaims");
+            apie.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public static void printPVCs(V1PersistentVolumeClaimList list){
+        System.out.println("----- PVCs ----");
+        String template = "%-16s\t%-40s\t%-6s%n";
+        System.out.format(template,"Name", "Volume", "Size");
+        
+        for (V1PersistentVolumeClaim item : list.getItems()) {
+            String name = item.getMetadata().getName();
+            String volumeName = item.getSpec().getVolumeName();
+            String size = item.getSpec().getResources().getRequests().get("storage");
+            System.out.format(template,name, volumeName, size);
+        }
+    }
+}
